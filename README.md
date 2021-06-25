713b1e7a3f85c7f64db255759202a5316427a717
Patrick Lee <plgagnon00@gmail.com>
2021-06-25 12:55:24 -0400

    First Push after Move from Deprecated "Cerner Data Syndication API" project

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Cerner-HealtheIntent
 HealtheIntent open development services allow access to population health concepts using RESTful APIs.
+
+This repository contains a Python wrapper for a number of Cerner Open Code APIs
+1. Cerner Data Syndication API - https://docs.healtheintent.com/api/v1/data_syndication/
+2. Organizations API - https://docs.healtheintent.com/api/v1/organization/
+3. Personnel API - https://docs.healtheintent.com/api/v1/personnel/
+
+
+## Definitions
+
+**Feeds**
+: The set of data that you want to receive on a schedule, for example, Longitudinal Record for population ABC.
+
+**Channels**
+: Specifies that you want to receive a feed’s bundles using a particular channel type and provides any necessary configuration for the channel type. For example, a channel could specify that you want to receive your Longitudinal Record for Population ABC feed using the DOWNLOAD channel type.
+
+**Bundles**
+: The output of a feed, for example, Changes to the Longitudinal Record for population ABC between 2018-04-01 and 2018-04-02.
+
+**Deliveries**
+: The status of delivering a particular bundle using a particular channel. For example, you would use the Deliveries endpoint to determine whether the archive file for changes to the Longitudinal Record for population ABC between 2018-04-01 and 2018-04-02 bundle is available for you to download.
+
+**Location**
+: A location represents a physical place with a single physical address, for example, a laboratory facility, practice site, hospital, or even a department within a hospital associated with an organization. Currently, personnel organizations and locations are not related in the API, but they will be in the future.
+
+**Organization**
+: A formally or informally recognized group of people or organizations formed for the purpose of achieving a collective action, for example, companies, institutions, corporations, departments, community groups, and health care practice groups.
+
+**Organization Group**
+: Organization groups are sets of organizations that are grouped for a common purpose. For example, organizations can be grouped to apply a consistent scoring methodology across organizations of the same type, filter a report to a certain set of organizations, or display a group of organizations together in applications.
+
+**Personnel and Organization Relationship**
+: A personnel can be a member or administrator of an organization, and some personnel may be both members and administrators of the same organization. Organization members typically are personnel who provide health care services to patients and whose financial performance and quality metrics contribute to the overall performance of the organization. Administrators are personnel who monitor and manage the activities and performance of their organizations.
+
+**Personnel**
+: Personnel are the people involved in the health care of a population. In most cases, personnel are employees or affiliates of a health care system or provider.
+: Users are personnel who log in to HealtheIntent applications. All users must be personnel, but not all personnel are necessarily users. Users gain access to features and information in HealtheIntent applications through membership in personnel groups and by being a member or administrator of organizations.
+
+**Personnel Group**
+: Personnel groups are sets of personnel who are grouped together to give them access to the same features or information. A personnel can belong to as many or as few personnel groups as necessary.
---
+++ healtheintent-api.py
@@ -0,0 +1,143 @@
+if __name__ == '__main__':
+    from settings import *
+else:
+    from .settings import *
+import requests
+import tarfile
+import datetime as dt
+import os
+
+
+class Syndication(object):
+
+    def __init__(self):
+
+        self.uri = URI
+        self.headers = BEARER_HEADER
+        self.channel_id = None
+        self.feed_id = None
+        self.personnel_id = None
+        self.organization_id = None
+        self.personnel_group = None
+        self.personnel_group_member_id = None
+        self.location_id = None
+
+    def __send_request__(self, endpoint, method = None, json = None, headers = None, params = None):
+        if method == 'GET':
+            return requests.get('{uri}{endpoint}'.format(uri=self.uri, endpoint=endpoint), headers=headers,
+                                params=params).json()
+        else:
+            return requests.get('{uri}{endpoint}'.format(uri=self.uri, endpoint=endpoint), headers=headers,
+                                    json=json)
+
+    # DATA SYNDICATION
+    def get_feeds(self):
+        return self.__send_request__(endpoint='/feeds', method='GET', headers=self.headers)
+
+    def get_channels(self):
+        return self.__send_request__(endpoint='/channels', method='GET', headers=self.headers)
+
+    def get_channel_status(self, channel_id = None):
+        return self.__send_request__(endpoint='/channels/{channel_id}'.format(channel_id=channel_id), method='GET',
+                                     headers=self.headers)
+
+    def get_channel_deliveries(self, channel_id = None):
+        return self.__send_request__(endpoint='/channels/{channel_id}/deliveries'.format(channel_id=channel_id),
+                                     method='GET', headers=self.headers)
+
+    def get_channel_downloads(self, delivery_id = None, destination_path = None):
+        response = self.__send_request__(endpoint='/downloads/{delivery_id}'.format(delivery_id=delivery_id)
+                                         , headers=self.headers)
+
+
+        wd = os.getcwd()
+        path = destination_path + '/' + dt.datetime.strftime(dt.datetime.today(), '%Y-%m-%d') + '.tar.gz'
+
+        if response.status_code == 200:
+            with open(path, 'wb') as i:
+                i.write(response.content)
+        i.close()
+
+        temp = tarfile.open(name=path, mode='r:gz')
+        os.chdir(destination_path)
+        temp.extractall()
+        temp.close()
+        os.chdir(wd)
+
+    # IDENTITY DOMAIN
+    # PERSONNEL
+    def get_personnel(self):
+        return self.__send_request__(endpoint='/personnel', method='GET', headers=self.headers)
+
+    def create_personnel(self): # INCOMPLETE
+        return self.__send_request__(endpoint='/personnel', method='POST', headers=self.headers)
+
+    def get_personnel_status(self, personnel_id = None):
+        return self.__send_request__(endpoint='/personnel/{personnel_id}'.format(personnel_id=personnel_id), method='GET', headers=self.headers)
+
+    def update_personnel(self): # INCOMPLETE
+        return self.__send_request__(endpoint='/personnel', method='PUT', headers=self.headers)
+
+    def delete_personnel(self, personnel_id = None):
+        return self.__send_request__(endpoint='/personnel/{personnel_id}'.format(personnel_id=personnel_id), method='DELETE', headers=self.headers)
+
+    #PERSONNEL GROUPS
+    def get_personnel_group(self):
+        return self.__send_request__(endpoint='/personnel-groups', method='GET', headers=self.headers)
+
+    def create_personnel_group(self): # INCOMPLETE
+        return self.__send_request__(endpoint='/personnel-groups', method='POST', headers=self.headers)
+
+    def get_personnel_group_status(self, personnel_group_id = None):
+        return self.__send_request__(endpoint='/personnel-groups/{personnel_group_id}'.format(personnel_group_id=personnel_group_id)
+                                     , method='GET'
+                                     , headers=self.headers)
+
+    def update_personnel_group(self, personnel_group_id = None): #INCOMPLETE
+        return self.__send_request__(endpoint='/personnel-groups/{personnel_group_id}'.format(personnel_group_id=personnel_group_id)
+                                     , method='PUT'
+                                     , headers=self.headers)
+
+    def delete_personnel_group(self, personnel_group_id = None):
+        return self.__send_request__(endpoint='/personnel-groups/{personnel_group_id}'.format(personnel_group_id=personnel_group_id)
+                                     , method='DELETE'
+                                     , headers=self.headers)
+
+    def get_personnel_group_members(self, personnel_group_id = None):
+        return self.__send_request__(endpoint='/personnel-groups/{personnel_group_id}/members'.format(personnel_group_id=personnel_group_id)
+                                     , method='GET'
+                                     , headers=self.headers)
+
+    def add_personnel_group_members(self, personnel_group_id = None, personnel_group_member_id = None):
+        return self.__send_request__(endpoint='/personnel-groups/{personnel_group_id}/members/{personnel_group_member_id}'
+                                     , method='PUT', headers=self.headers)
+
+    def delete_personnel_group_members(self, personnel_group_id = None, personnel_group_member_id = None):
+        return self.__send_request__(endpoint='/personnel-groups/{personnel_group_id}/members/{personnel_group_member_id}'
+                                     , method='DELETE', headers=self.headers)
+
+
+    # ORGANIZATIONS
+    def get_locations(self):
+        return self.__send_request__(endpoint='/locations', method='GET', headers=self.headers)
+
+    def create_location(self):
+        return self.__send_request__(endpoint='/locations', method='POST', headers=self.headers)
+
+    def get_location_status(self, location_id = None):
+        return self.__send_request__(endpoint='/locations/{location_id}'.format(location_id=location_id)
+                                     , method='GET', headers=self.headers)
+
+    def update_location(self, location_id = None):
+        return self.__send_request__(endpoint='/locations/{location_id}'.format(location_id=location_id)
+                                     , method='PUT', headers=self.headers)
+
+    def delete_location(self, location_id = None):
+        return self.__send_request__(endpoint='/locations/{location_id}'.format(location_id=location_id)
+                                     , method='DELETE', headers=self.headers)
+
+
+if __name__ == '__main__':
+    s = Syndication()
+    s.get_channel_downloads(delivery_id=banana,destination_path=TEST_PATH)
+
